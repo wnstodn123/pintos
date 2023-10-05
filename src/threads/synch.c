9fee880ed4989b6b40bd634f312f47dd27088bc5
wnstodn123 <wnstodn123@postech.ac.kr>
2023-10-05 14:51:33 +0900

    lock_release 함수 수정

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -239,6 +239,10 @@ lock_release (struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
 
   lock->holder = NULL;
+
+  remove_with_lock(lock);
+  refresh_priority();
+
   sema_up (&lock->semaphore);
 }
 
