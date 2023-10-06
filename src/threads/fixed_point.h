eca7d8815fdd1bd1187166419932943fea403fae
sseung45 <sseung45@gmail.com>
2023-10-06 20:25:00 +0900

    fixed-point 연산 함수 frame 작성

---
+++ src/threads/fixed_point.h
@@ -0,0 +1,14 @@
+#define INT_MAX ((1<<31)-1)
+#define INT_MIN (-(1<<31))
+
+int int_to_fp(int n);
+int fp_to_int_tozero(int x);
+int fp_to_int_nearest(int x);
+int fp_add_fp(int x, int y);
+int fp_sub_fp(int x, int y);
+int fp_add_int(int x, int n);
+int fp_sub_int(int x, int n);
+int fp_mul_fp(int x, int y);
+int fp_mul_int(int x, int n);
+int fp_div_fp(int x, int y);
+int fp_div_int(int x, int n);
