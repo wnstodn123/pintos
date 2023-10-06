c94874768088fd89ed192bce2134108f11fcf0f2
sseung45 <sseung45@gmail.com>
2023-10-06 20:35:20 +0900

    fixed-point 연산 함수 기능 구현

--- src/threads/fixed_point.h
+++ src/threads/fixed_point.h
@@ -13,3 +13,15 @@ int fp_mul_fp(int x, int y);
 int fp_mul_int(int x, int n);
 int fp_div_fp(int x, int y);
 int fp_div_int(int x, int n);
+
+int int_to_fp(int n) { return n * F; }
+int fp_to_int_tozero(int x) { return x / F; }
+int fp_to_int_nearest(int x) { return x >= 0 ? (x + F / 2)/2 : (x - F / 2)/2; }
+int fp_add_fp(int x, int y) { return x + y; }
+int fp_sub_fp(int x, int y) { return x - y; }
+int fp_add_int(int x, int n) { return x + n * F; }
+int fp_sub_int(int x, int n) { return x - n * F; }
+int fp_mul_fp(int x, int y) { return ((int64_t)x) * y / F; }
+int fp_mul_int(int x, int n) { return x * n; }
+int fp_div_fp(int x, int y) { return ((int64_t)x) * F / y; }
+int fp_div_int(int x, int n) { return x / n; }
